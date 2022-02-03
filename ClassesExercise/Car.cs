64d189476457529df4f8a537dbe261dc26137539
Matthew Bonner <matthewsbonner@comcast.net>
2022-02-03 13:30:27 -0500

    initial commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set;  }
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car("Dodge", "Dakota", 1997);
+
+            Console.WriteLine($"Make:{myCar.Make} Model:{myCar.Model} Year{myCar.Year}");
+        }
+    }
+}
